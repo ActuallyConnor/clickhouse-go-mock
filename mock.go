8dada34381cd128fc6558367009faf9fe5f39b2a
connorsmyth <connor.smyth@noibu.com>
2025-07-04 12:26:01 -0400

    batman

---
+++ mock.go
@@ -0,0 +1,75 @@
+package mock
+
+import (
+	"context"
+	"fmt"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/proto"
+	"time"
+)
+
+// ConnMock is a mock implementation of clickhouse driver.Conn interface
+type ConnMock struct {
+}
+
+func (c ConnMock) ServerVersion() (*driver.ServerVersion, error) {
+	utc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		fmt.Println("err: ", err.Error())
+	}
+	return &driver.ServerVersion{
+		Name:        "name",
+		DisplayName: "display-name",
+		Revision:    0,
+		Version: proto.Version{
+			Major: 0,
+			Minor: 0,
+			Patch: 0,
+		},
+		Timezone: utc,
+	}, nil
+}
+
+func (c ConnMock) Select(ctx context.Context, dest any, query string, args ...any) error {
+	return nil
+}
+
+func (c ConnMock) Query(ctx context.Context, query string, args ...any) (driver.Rows, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c ConnMock) QueryRow(ctx context.Context, query string, args ...any) driver.Row {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c ConnMock) PrepareBatch(ctx context.Context, query string, opts ...driver.PrepareBatchOption) (driver.Batch, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c ConnMock) Exec(ctx context.Context, query string, args ...any) error {
+	return nil
+}
+
+func (c ConnMock) AsyncInsert(ctx context.Context, query string, wait bool, args ...any) error {
+	return nil
+}
+
+func (c ConnMock) Ping(ctx context.Context) error {
+	return nil
+}
+
+func (c ConnMock) Stats() driver.Stats {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c ConnMock) Close() error {
+	return nil
+}
+
+func (c ConnMock) Contributors() []string {
+	return make([]string, 0)
+}
